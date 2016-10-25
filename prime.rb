11c2cc9aaac02458d85c18631daddd91a58b4a0a
p sc <patty.stacruz@gmail.com>
2016-10-25 23:44:33 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+def prime?(number)
+  if number < 2
+    return false
+  end
+
+  index = 2
+  while index < number
+    if (number % index) == 0
+      return false
+    end
+    index += 1
+  end
+
+  return true
+end
